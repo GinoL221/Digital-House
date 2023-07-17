8a1022213ffed95d5ac3359738f6fa06c4754591
GinoL221 <ginolencina@gmail.com>
2023-07-17 18:56:49 -0300

    clase 9

--- Modulo 1/clase9/desafio/app.js
+++ Modulo 1/clase9/desafio/app.js
@@ -1,7 +1,7 @@
 const peliculas = require("./proyecto/movies");
-//console.log(peliculas[1]);
+console.log(peliculas[1]);
 
 let fs = require("fs")
-let message = fs.readFileSync(__dirname + "/message.txt", "utf-8");
+let message = fs.readFileSync(__dirname + "/proyecto/message.txt", "utf-8");
 
 console.log(message);
