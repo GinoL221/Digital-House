7ca3ce7e854bbe4ba216eb73533348fd53e4d222
GinoL221 <ginolencina@gmail.com>
2023-08-23 19:21:56 -0300

    Corrección

--- Modulo 1/clase11/desafio/app.js
+++ Modulo 1/clase11/desafio/app.js
@@ -10,15 +10,20 @@ const data3 = importar("Star Wars");
 const unifiedCollectibles = [...data1, ...data2, ...data3];
 
 const collectibles = {
-    // Desafio 2 - 4
-    figuras: unifiedCollectibles,
-    // Desafio 2 - 5.a
-    listFigures: function () { unifiedCollectibles.forEach(collectable => console.log(collectable)) },
-    // Desafio 2 - 5.b
-    figuresByBrand: function (brand) {
-        return unifiedCollectibles.filter(collectable => collectable.marca.trim().toUpperCase() == brand.trim().toUpperCase())
-    }
-}
+  // Desafio 2 - 4
+  figuras: unifiedCollectibles,
+  // Desafio 2 - 5.a
+  listFigures: function () {
+    unifiedCollectibles.forEach((collectable) => console.log(collectable));
+  },
+  // Desafio 2 - 5.b
+  figuresByBrand: function (brand) {
+    return unifiedCollectibles.filter(
+      (collectable) =>
+        collectable.marca.trim().toUpperCase() == brand.trim().toUpperCase()
+    );
+  },
+};
 
 // Desafio 2 - 5.b
 // collectibles.listFigures();
