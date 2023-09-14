acf34a6f3f26fc19890954a28aea6d0108a3f1ce
GinoL221 <ginolencina@gmail.com>
2023-09-14 08:22:39 -0300

    Reubicacion de carpetas

---
+++ Modulo 5/app-mvc-SMI3/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productsRoutes");
+
+const server = express();
+
+server.use(morgan("dev"));
+server.set("view engine", "ejs");
+
+server.use(express.static(path.join(__dirname, "../public")));
+
+server.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/html/landing.html"))
+);
+
+server.use(userRoutes);
+server.use(productRoutes);
+
+module.exports = server;
